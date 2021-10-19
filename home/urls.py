3d6a4dfd90d3a7c5de741b1703da19d272928087
Aurélien Fortin <aurelien@MacBook-Air-de-Aurelien.local>
2021-10-19 11:07:47 +0200

    test

--- home/urls.py
+++ home/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('index', views.index, name = 'index'),
     path('evenements', views.evenements, name = 'evenements'),
     path('team', views.team, name = 'team'),
-    path('tools', views.tools, name = 'tools'),
+    path('outils', views.outils, name = 'tools'),
     path('pres', views.pres, name = 'pres'),
 ]
